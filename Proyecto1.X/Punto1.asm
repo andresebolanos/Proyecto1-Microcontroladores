955a8d48609b7b835513d853420f3e56cf79a7a4
Andres Bolanos <andresebolanos@unicauca.edu.co>
2026-02-25 12:40:51 -0500

    Solucioné oscilador de 1 segundo con Timer0

--- Proyecto1.X/Punto1.asm
+++ Proyecto1.X/Punto1.asm
@@ -10,7 +10,9 @@
     config WDT    = OFF	        ; Desactiva el Watchdog Timer
     config LVP    = OFF	        ; Deshabilita la programaci?n en bajo voltaje
     config MCLRE  = OFF		; Contrala si el pin MCLR es: ON: Pin de Reset, OFF: Se convierte en entrada digital
-    CONFIG  PWRT   = ON	        ; Es el Power-ip Timer: ON:Agrega un pequeño retardo cuando el PIC enciende, OFF: Arranca inmediatamente
+    CONFIG PWRT   = ON	        ; Es el Power-ip Timer: ON:Agrega un pequeño retardo cuando el PIC enciende, OFF: Arranca inmediatamente
+    CONFIG PBADEN = OFF
+    CONFIG DEBUG  = OFF
     
     ;=== Vectores de Inicio ===
     PSECT resetVec, class=CODE, reloc=2 ; Secci?n para el vector de reinicio
@@ -22,35 +24,36 @@ resetVec:
     PSECT main_code, class=CODE, reloc=2  ; Secci?n de c?digo principal
     
 Inicio:
-    MOVLW 0x72
+    MOVLW 0b01110010
     MOVWF OSCCON, A
-    CLRF TRISD, A	    ; PORTD como salida
-    CLRF LATD, A	    ; Apagar los pines de PORTD
+    BCF   TRISD, 0, A	    ; PORTD(0) como salida
+    BSF   LATD, 0, A	    ; Apagar pin 0 de PORTD
     
     ; Configurar Timer0: 16 bits, preescaler 1:256, reloj interno
     MOVLW   0x87
-    MOVWF   T0CON, b
+    MOVWF   T0CON, A
    
     BCF     INTCON, 2, A ; Limpiar bandera
     BSF     T0CON,  7, A ; Timer0 = ON
     
 Loop:
-    BTG LATD, 0, A ; Cambiar el estado del LED
+    CALL Espera
+    BTG LATD , 0, A ; Cambiar el estado del LED
     GOTO Loop
 
 Espera:
     ; Recarga del timer para 1 seg
     ; Con Fosc=8Mhz, tenemos un Tcy=0.5us y usamos un preescaler de 256
     ; 1seg/(0.5us*256)= 7812 ticks -> calculo de precarga 65536-7812=57724=0xE17C
-    BCF   T0CON, 7, b  ; Detener timer
+    BCF   T0CON, 7, A  ; Detener timer
     
     MOVLW 0XE1
-    MOVWF TMR0H, b
+    MOVWF TMR0H, A
     MOVLW 0X7C 
-    MOVWF TMR0L, b
+    MOVWF TMR0L, A
     
     BCF   INTCON, 2, A ; Limpiar bandera
-    BSF   T0CON,  7, b ; Iniciar timer
+    BSF   T0CON,  7, A ; Iniciar timer
     
     ;Esperar desbordamiento
 EsperaLoop:
