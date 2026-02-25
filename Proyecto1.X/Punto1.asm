0f7cfe8092cdc01bcfe9ecd44f7c289978acd397
Andres Bolanos <andresebolanos@unicauca.edu.co>
2026-02-25 00:48:08 -0500

    Versión 1: Intento de implementar Timer de 1 segundo para el punto 1

---
+++ Proyecto1.X/Punto1.asm
@@ -0,0 +1,65 @@
+;==================================================================
+;Codigo en Assembler para PIC18F4550
+;Usa retardos con Timer0
+;Frecuencia: 8MHz
+;==================================================================
+    #include <xc.inc>  
+    
+    ; Configuracion de bits de confiuracion
+    config FOSC   = INTOSCIO_EC ; Usa el oscilador interno a 8MHz
+    config WDT    = OFF	        ; Desactiva el Watchdog Timer
+    config LVP    = OFF	        ; Deshabilita la programaci?n en bajo voltaje
+    config MCLRE  = OFF		; Contrala si el pin MCLR es: ON: Pin de Reset, OFF: Se convierte en entrada digital
+    CONFIG  PWRT   = ON	        ; Es el Power-ip Timer: ON:Agrega un pequeño retardo cuando el PIC enciende, OFF: Arranca inmediatamente
+    
+    ;=== Vectores de Inicio ===
+    PSECT resetVec, class=CODE, reloc=2 ; Secci?n para el vector de reinicio
+resetVec:
+    ORG 0x00				; Direccion de inicio
+    GOTO Inicio				; Saltar a la rutina de inicio
+    
+    ;=== Codigo Principal ===
+    PSECT main_code, class=CODE, reloc=2  ; Secci?n de c?digo principal
+    
+Inicio:
+    MOVLW 0x72
+    MOVWF OSCCON, A
+    CLRF TRISD, A	    ; PORTD como salida
+    CLRF LATD, A	    ; Apagar los pines de PORTD
+    
+    ; Configurar Timer0: 16 bits, preescaler 1:256, reloj interno
+    MOVLW   0x87
+    MOVWF   T0CON, b
+   
+    BCF     INTCON, 2, A ; Limpiar bandera
+    BSF     T0CON,  7, A ; Timer0 = ON
+    
+Loop:
+    BTG LATD, 0, A ; Cambiar el estado del LED
+    GOTO Loop
+
+Espera:
+    ; Recarga del timer para 1 seg
+    ; Con Fosc=8Mhz, tenemos un Tcy=0.5us y usamos un preescaler de 256
+    ; 1seg/(0.5us*256)= 7812 ticks -> calculo de precarga 65536-7812=57724=0xE17C
+    BCF   T0CON, 7, b  ; Detener timer
+    
+    MOVLW 0XE1
+    MOVWF TMR0H, b
+    MOVLW 0X7C 
+    MOVWF TMR0L, b
+    
+    BCF   INTCON, 2, A ; Limpiar bandera
+    BSF   T0CON,  7, b ; Iniciar timer
+    
+    ;Esperar desbordamiento
+EsperaLoop:
+    BTFSS INTCON, 2, A
+    GOTO EsperaLoop
+    BCF INTCON, 2, A 
+    Return
+    
+    ;=== Variables ===
+    PSECT udata 
+Contador: DS 1
+    END
